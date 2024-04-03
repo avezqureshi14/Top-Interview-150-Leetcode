fcc35594d8cb4562146121020045f0845348566e
avezqureshi <aveznqureshi14@gmail.com>
2024-04-03 07:07:41 +0530

    merge sort

--- 01-merge-sorted-array.cpp
+++ 01-merge-sorted-array.cpp
@@ -2,8 +2,49 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+void merge(vector<int> &nums1, int m, vector<int> &nums2, int n)
+{
+    int t = m + n;
+    int i = m - 1, j = n - 1;
+    while (i >= 0 && j >= 0)
+    {
+        if (nums1[i] >= nums2[j])
+        {
+            nums1[t - 1] = nums1[i];
+            i--;
+            t--;
+        }
+        else
+        {
+            nums1[t - 1] = nums2[j];
+            j--;
+            t--;
+        }
+    }
+    while (i >= 0)
+    {
+        nums1[t - 1] = nums1[i];
+        i--;
+        t--;
+    }
+    while (j >= 0)
+    {
+        nums1[t - 1] = nums2[j];
+        j--;
+        t--;
+    }
+}
+
 int main()
 {
-    cout << "Hello World";
+    vector<int> nums1 = {1, 2, 3, 0, 0, 0};
+    vector<int> nums2 = {2, 5, 6};
+    int m = 3;
+    int n = 3;
+    merge(nums1, m, nums2, n);
+    for (auto i : nums1)
+    {
+        cout << i << " ";
+    }
     return 0;
 }
